772684f5310ec376c796cc60bc6bf8960b481bf8
intuit07 <intuit07@gmail.com>
2018-10-29 22:32:13 +0200

    add yobit, make answer only on last message

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 import requests
 import misc
-import json
+
+from time import sleep
+
+from yobit import get_btc
 
 token = misc.token
 
 # https://api.telegram.org/bot716310445:AAEHoOyWSyOBWcbZdiD6c5LEESxKD8Z3_fA/sendmessage?chat_id=275330781&text=test
 URL = 'https://api.telegram.org/bot' + token + '/'
-# print('URL -->', URL)
+
+global last_update_id
+last_update_id = 0
+
+
 
 
 def get_updates():
@@ -16,32 +23,43 @@ def get_updates():
 
 
 def get_message():
+    #отвечать только на новые сообщения
+    # получаем update_id, каждого обновления
+    # записываем в переменную, а затем сравнить
+    # с update_id последнего елемента в списке result
+
     data = get_updates()
-    chat_id = data['result'][-1]['message']['chat']['id']
-    message_text = data['result'][-1]['message']['text']
-    message = {'chat_id': chat_id,
-               'message_text': message_text}
-    return message
+    last_object = data['result'][-1]
+    current_update_id = last_object['update_id']
+
+    global last_update_id
+    if last_update_id != current_update_id:
+        chat_id = data['result'][-1]['message']['chat']['id']
+        message_text = data['result'][-1]['message']['text']
+        message = {'chat_id': chat_id,
+                   'text': message_text}
+        return message
+    return None
 
 
 def send_message(chat_id, text='Подождите секундочку пожалуйста...'):
     url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
     requests.get(url)
-    print(url)
 
 
 def main():
-    # d = get_updates()
 
-    # with open('updates.json', 'w') as file:
-    #     json.dump(d, file, indent=2, ensure_ascii=False)
-    # print(send_message(13))
-    answer = get_message()
-    chat_id = answer['chat_id']
+    while True:
+        answer = get_message()
+        if answer != None:
+            chat_id = answer['chat_id']
+            text = answer['text']
+            if text == '/btc':
+                send_message(chat_id, get_btc())
+        else:
+            continue
 
-    send_message(chat_id, 'Что ты хочешь на ужин?')
-
-
+            sleep(3)
 
 if __name__ == '__main__':
     main()
